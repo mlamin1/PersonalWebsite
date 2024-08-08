721098705e76113569a9fbc02c04a3822436c033
Muniru Lamin <laminmuniru@yahoo.com>
2024-08-08 17:50:09 -0400

    Fix typo in WhiteNoise middleware

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.niddleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'personal_website.urls'
