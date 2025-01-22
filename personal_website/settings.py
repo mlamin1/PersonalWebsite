1be5c002dcfd3edffff4dee08a0eab17bbfd7230
Muniru Lamin <laminmuniru@yahoo.com>
2025-01-22 00:13:34 -0500

    Added changes for the project page

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -158,6 +158,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Add these settings after the STATIC_ROOT configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
