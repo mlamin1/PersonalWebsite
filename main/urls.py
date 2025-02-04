05bbe7e9c92d1da04cccaa32c35a92293233cc39
Muniru Lamin <laminmuniru@yahoo.com>
2025-02-03 20:45:33 -0500

    Added contact page with form and social links

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about_me, name='about_me'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+] 
