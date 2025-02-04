f4379eaa75ced53f21d6841953e9649a99fb134f
Muniru Lamin <laminmuniru@yahoo.com>
2025-02-03 22:00:31 -0500

    Update email settings to use SSL

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -171,8 +171,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.yahoo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'laminmuniru@yahoo.com'
 EMAIL_HOST_PASSWORD = ''  # Set this in Heroku config vars
 DEFAULT_FROM_EMAIL = 'laminmuniru@yahoo.com'
